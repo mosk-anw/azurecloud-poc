d5674230f805b85eb97ba4e47301b71291fc5eb7
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 00:04:10 +0100

    feat: Add terraform config for 'create a kubernetes cluster two node in south region'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = "~> 3.0"
     }
   }
 
@@ -14,8 +14,35 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "testrg"
-  location = "UK South"
+resource "azurerm_resource_group" "new_rg" {
+  name     = "new-rg"
+  location = "South India"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_test01" {
+  name                = "aks-test01"
+  location            = azurerm_resource_group.new_rg.location
+  resource_group_name = azurerm_resource_group.new_rg.name
+  dns_prefix          = "aks-test01-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  kubernetes_version = "1.22.4" # Example version, replace with the actual stable version if necessary
+
+  lifecycle {
+    ignore_changes = [kubernetes_version]
+  }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks_test01.kube_config_raw
 }
 ```
