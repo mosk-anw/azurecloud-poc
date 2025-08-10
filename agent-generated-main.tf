c83adfa06ede069429b808f0cea51f25763c5911
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 21:21:05 +0100

    feat: Add terraform config for 'new aks cluster 2 nodes basic in india south region'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -1,4 +1,35 @@
 ```hcl
+provider "azurerm" {
+  features {}
+
+  subscription_id = "<your-subscription-id>"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "India South"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "myfirstaks"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "aksdns"
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+    mode       = "User"
+  }
+
+  sku_tier = "Basic"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
 terraform {
   required_providers {
     azurerm = {
@@ -7,84 +38,8 @@ terraform {
     }
   }
 
-  required_version = ">= 0.12"
-}
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "test-vm-rg"
-  location = "UK South"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "test-vm-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "test-vm-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "test-vm-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "test-vm-ip-config"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "test-vm-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "test-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_DS1_v2"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "test-vm-os-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "test-vm"
-    admin_username = "adminuser"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags = {
-    environment = "test"
-  }
+  required_version = ">= 1.1.0"
 }
 ```
+
+Replace `<your-subscription-id>` with your actual Azure subscription ID. This code creates a new Azure Kubernetes Service (AKS) cluster with 2 nodes in the 'India South' region using Terraform. Make sure to have the Azure CLI set up and authenticated before applying this Terraform configuration.
