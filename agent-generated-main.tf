1a095d0128a4cbeec12a55b8dd273af2b0315970
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 20:56:09 +0100

    feat: Add terraform config for 'crate new vm '

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -7,57 +7,57 @@ terraform {
     }
   }
 
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.12"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "rg" {
+  name     = "test-vm-rg"
   location = "UK South"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "test-vm-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_subnet" "subnet" {
+  name                 = "test-vm-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "nic" {
+  name                = "test-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    name                          = "test-vm-ip-config"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_virtual_machine" "example" {
-  name                  = "linuxvm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_B1ms"
+resource "azurerm_public_ip" "public_ip" {
+  name                = "test-vm-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
 
-  storage_os_disk {
-    name              = "osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "test-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
     publisher = "Canonical"
@@ -66,10 +66,17 @@ resource "azurerm_virtual_machine" "example" {
     version   = "latest"
   }
 
+  storage_os_disk {
+    name              = "test-vm-os-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
   os_profile {
-    computer_name  = "linuxvm"
-    admin_username = "azureuser"
-    admin_password = "P@ssw0rd1234!"
+    computer_name  = "test-vm"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
   }
 
   os_profile_linux_config {
@@ -77,7 +84,7 @@ resource "azurerm_virtual_machine" "example" {
   }
 
   tags = {
-    environment = "Test"
+    environment = "test"
   }
 }
 ```
