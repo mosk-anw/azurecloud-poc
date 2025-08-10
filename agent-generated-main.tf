ed8114a94fe4c24e77831f3ee735eabb6b56382d
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 08:17:57 +0100

    feat: Add terraform config for 'Create a new resource group.'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -1,10 +1,25 @@
 ```hcl
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "motest" {
-  name     = "motest"
+resource "azurerm_resource_group" "test" {
+  name     = "test-rg"
   location = "UK South"
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.test.id
 }
 ```
