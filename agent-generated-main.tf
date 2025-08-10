ffd911d781c4cdb8372bb9c85582efe7cdbb6164
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 09:41:38 +0100

    feat: Add terraform config for 'create a new storage accoutn for storing my phootos '

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -3,19 +3,27 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = ">= 3.0"
     }
   }
 
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "test-rg"
+resource "azurerm_resource_group" "rg_storage" {
+  name     = "rg-storage"
   location = "UK South"
+}
+
+resource "azurerm_storage_account" "test_storage" {
+  name                     = "teststorageacct"
+  resource_group_name      = azurerm_resource_group.rg_storage.name
+  location                 = azurerm_resource_group.rg_storage.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 ```
