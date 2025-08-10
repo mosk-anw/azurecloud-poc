0ab1158eaf2752ec2eb10ae91307fc4b808380d5
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 09:38:27 +0100

    feat: Add terraform config for 'create a new rg '

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
 
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
@@ -15,67 +15,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "rg-mysite"
-  location = "South India"
-}
-
-resource "azurerm_network_security_group" "example" {
-  name                = "nsg-mysite"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "vnet-mysite"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "subnet-mysite"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "pip-mysite"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "nic-mysite"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "mysite"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-  size            = "Standard_B1s"
-  admin_username  = "adminuser"
-  admin_password  = "P@ssword1234!!"
-  disable_password_authentication = false
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+  name     = "test-rg"
+  location = "UK South"
 }
 ```
