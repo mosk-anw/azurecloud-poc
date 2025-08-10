b795f2ccf4bc7ab2bd4cbffd32d072f3896bbc8b
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 10:09:06 +0100

    feat: Add terraform config for 'new web iss server'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -6,20 +6,109 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  required_version = ">= 0.12"
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "testrg" {
-  name     = "testrg"
+resource "azurerm_resource_group" "example" {
+  name     = "testvm-rg"
   location = "UK South"
 }
 
-output "resource_group_id" {
-  value = azurerm_resource_group.testrg.id
+resource "azurerm_virtual_network" "example" {
+  name                = "testvm-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "testvm-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "testvm-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "testvm-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testvm-ipconfig"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "testvm-pip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "testvm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_B1s"
+  admin_username      = "testadmin"
+  admin_password      = "P@ssw0rd1234"
+
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "null_resource" "iis_install" {
+  provisioner "remote-exec" {
+    connection {
+      type     = "winrm"
+      user     = "testadmin"
+      password = "P@ssw0rd1234"
+      host     = azurerm_public_ip.example.ip_address
+      timeout  = "15m"
+    }
+
+    inline = [
+      "powershell.exe -Command \"Install-WindowsFeature -name Web-Server\"",
+      "powershell.exe -Command \"Start-Service w3svc\""
+    ]
+  }
 }
 ```
