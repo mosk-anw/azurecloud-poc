77518dd39a1450baacd2ebcc4bcf93e1262bc47d
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 09:59:26 +0100

    feat: Add terraform config for 'create a postgres instance '

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -3,120 +3,61 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = "~> 3.0"
     }
   }
-
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
+variable "prefix" {
+  description = "The prefix used for all resources in this example."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  type        = string
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "webserver-rg"
-  location = "India South"
+  name     = "${var.prefix}-rg"
+  location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "webserver-vnet"
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_postgresql_server" "postgresql" {
+  name                = "${var.prefix}-pgserver"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  administrator_login = "pgadmin"
+  administrator_login_password = "H@Sh1CoR3!"
+  version             = "11"
+  sku_name            = "B_Gen5_2"
+  storage_mb          = 5120
+  backup_retention_days = 7
+  geo_redundant_backup_enabled = true
+  public_network_access_enabled = true
+  ssl_enforcement_enabled = true
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "webserver-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "webserver-public-ip"
-  location            = azurerm_resource_group.rg.location
+resource "azurerm_postgresql_database" "example" {
+  name                = "exampledb"
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  server_name         = azurerm_postgresql_server.postgresql.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
 }
 
-resource "azurerm_network_interface" "network_interface" {
-  name                = "webserver-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
+output "postgresql_server_name" {
+  description = "The name of the PostgreSQL server"
+  value       = azurerm_postgresql_server.postgresql.name
 }
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = "webserver-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_ranges    = ["22"]
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_ranges    = ["80"]
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "nsg_association" {
-  network_interface_id      = azurerm_network_interface.network_interface.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_linux_virtual_machine" "web_server" {
-  name                = "webvm01"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  network_interface_ids = [
-    azurerm_network_interface.network_interface.id,
-  ]
-  size               = "Standard_B1s"
-  admin_username     = "adminuser"
-  admin_password     = "P@ssw0rd1234!" 
-  disable_password_authentication = false
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  computer_name                   = "webvm01"
-  custom_data = base64encode("#!/bin/bash\napt-get update\napt-get install -y nginx\nsystemctl start nginx")
-
-  tags = {
-    environment = "Production"
-  }
+output "postgresql_fully_qualified_domain_name" {
+  description = "The FQDN of the PostgreSQL server"
+  value       = azurerm_postgresql_server.postgresql.fqdn
 }
 ```
