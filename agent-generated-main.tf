7bce81124920dcb47d3478f132d9ba550e4ea623
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 10:15:42 +0100

    feat: Add terraform config for 'creat new postgres db '

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -6,109 +6,56 @@ terraform {
       version = "~> 3.0"
     }
   }
-  required_version = ">= 1.1.0"
+
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "testvm-rg"
-  location = "UK South"
+variable "prefix" {
+  description = "The prefix used for all resources"
+  type        = string
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "testvm-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+variable "location" {
+  description = "The Azure Region in which all resources should be created"
+  type        = string
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "testvm-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-rg"
+  location = var.location
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "testvm-nsg"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_postgresql_flexible_server" "main" {
+  name                = "${var.prefix}-db01"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  administrator_login = "adminuser"
+  administrator_password = "P@ssw0rd1234!"
 
-  security_rule {
-    name                       = "RDP"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+  sku_name = "Standard_B1ms"
+  storage_mb = 10240
+
+  version = "13"
+
+  depends_on = [azurerm_resource_group.main]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "testvm-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "testvm-ipconfig"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
+resource "azurerm_postgresql_flexible_server_configuration" "main" {
+  name                = "backslash_quote"
+  server_id           = azurerm_postgresql_flexible_server.main.id
+  value               = "on"
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "testvm-pip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
+resource "azurerm_postgresql_flexible_server_database" "main" {
+  name     = "db01"
+  server_id = azurerm_postgresql_flexible_server.main.id
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "testvm"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  size                = "Standard_B1s"
-  admin_username      = "testadmin"
-  admin_password      = "P@ssw0rd1234"
-
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-}
-
-resource "null_resource" "iis_install" {
-  provisioner "remote-exec" {
-    connection {
-      type     = "winrm"
-      user     = "testadmin"
-      password = "P@ssw0rd1234"
-      host     = azurerm_public_ip.example.ip_address
-      timeout  = "15m"
-    }
-
-    inline = [
-      "powershell.exe -Command \"Install-WindowsFeature -name Web-Server\"",
-      "powershell.exe -Command \"Start-Service w3svc\""
-    ]
-  }
+output "postgresql_server_fqdn" {
+  value = azurerm_postgresql_flexible_server.main.fqdn
 }
 ```
