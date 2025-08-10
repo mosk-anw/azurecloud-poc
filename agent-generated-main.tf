6d0d74be2c6f4ad6e57d796ddbf2aae20a910d68
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 09:56:26 +0100

    feat: Add terraform config for 'create a new linux vm for webserver'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -3,120 +3,120 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = ">= 3.0.0"
     }
   }
 
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-variable "prefix" {
-  description = "A prefix used for all resources in this example"
-  type        = string
-}
-
-variable "password" {
-  description = "Service Principal password"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure Region in which all resources in this example should be provisioned"
-  type        = string
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-rg"
-  location = var.location
+  name     = "webserver-rg"
+  location = "India South"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
+  name                = "webserver-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/8"]
 }
 
-resource "azurerm_subnet" "aks_subnet" {
-  name                 = "${var.prefix}-aks-subnet"
+resource "azurerm_subnet" "subnet" {
+  name                 = "webserver-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.0.0/16"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_subnet" "aci_subnet" {
-  name                 = "${var.prefix}-aci-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.1.0.0/16"]
-
-  delegation {
-    name = "acidelegation"
-
-    service_delegation {
-      name = "Microsoft.ContainerInstance/containerGroups"
-
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/action",
-      ]
-    }
-  }
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "${var.prefix}-aks"
+resource "azurerm_public_ip" "public_ip" {
+  name                = "webserver-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  kubernetes_version  = "1.19.11"
-  dns_prefix          = "${var.prefix}aksdns"
+  allocation_method   = "Dynamic"
+}
 
-  default_node_pool {
-    name       = "agentpool"
-    node_count = 2
-    vm_size    = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.aks_subnet.id
+resource "azurerm_network_interface" "network_interface" {
+  name                = "webserver-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "webserver-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["22"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 
-  identity {
-    type = "SystemAssigned"
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["80"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_association" {
+  network_interface_id      = azurerm_network_interface.network_interface.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "web_server" {
+  name                = "webvm01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_ids = [
+    azurerm_network_interface.network_interface.id,
+  ]
+  size               = "Standard_B1s"
+  admin_username     = "adminuser"
+  admin_password     = "P@ssw0rd1234!" 
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 
-  network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "Standard"
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  addon_profile {
-    aci_connector_linux {
-      enabled     = true
-      subnet_name = azurerm_subnet.aci_subnet.name
-    }
-
-    oms_agent {
-      enabled = false
-    }
-  }
-
-  role_based_access_control {
-    enabled = true
-  }
+  computer_name                   = "webvm01"
+  custom_data = base64encode("#!/bin/bash\napt-get update\napt-get install -y nginx\nsystemctl start nginx")
 
   tags = {
-    environment = "Development"
+    environment = "Production"
   }
-}
-
-output "kube_config" {
-  description = "Kubectl configuration to access the cluster"
-  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
-
-output "kube_admin_config" {
-  description = "Admin Kubectl configuration to access the cluster"
-  value       = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
 }
 ```
