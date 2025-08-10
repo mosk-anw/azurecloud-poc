96e817dadb247d68f8d0e4383ddbcc665ed3b6f5
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 10:35:50 +0100

    feat: Add terraform config for 'creat a storage to store my documents in azure '

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = ">= 3.0"
     }
   }
 
@@ -14,35 +14,21 @@ provider "azurerm" {
   features {}
 }
 
-variable "prefix" {
-  description = "The prefix used for all resources in this example."
-  type        = string
+resource "azurerm_resource_group" "example" {
+  name     = "StorageResourceGroup"
+  location = "India"
 }
 
-variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
-  type        = string
-}
+resource "azurerm_storage_account" "example" {
+  name                     = "mystoage01"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Hot"
 
-resource "azurerm_cosmosdb_account" "ecomdb" {
-  name                = "${var.prefix}ecomdb"
-  location            = var.location
-  resource_group_name = "${var.prefix}-rg"
-
-  offer_type = "Standard"
-
-  kind  = "GlobalDocumentDB"
-  consistency_policy {
-    consistency_level = "Session"
-  }
-
-  geo_location {
-    location          = var.location
-    failover_priority = 0
-  }
-
-  capabilities {
-    name = "EnableServerless"
+  tags = {
+    environment = "Dev"
   }
 }
 ```
