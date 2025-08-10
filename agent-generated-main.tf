01bacdec04b86053a5e3f22a718f18777ad34d17
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 07:44:50 +0100

    feat: Add terraform config for 'Create a new resource group.'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -7,42 +7,23 @@ terraform {
     }
   }
 
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.1.0"
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "new_rg" {
-  name     = "new-rg"
-  location = "South India"
+resource "azurerm_resource_group" "mo_test" {
+  name     = "mo-test"
+  location = "UK South"
 }
 
-resource "azurerm_kubernetes_cluster" "aks_test01" {
-  name                = "aks-test01"
-  location            = azurerm_resource_group.new_rg.location
-  resource_group_name = azurerm_resource_group.new_rg.name
-  dns_prefix          = "aks-test01-dns"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_B2s"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  kubernetes_version = "1.22.4" # Example version, replace with the actual stable version if necessary
-
-  lifecycle {
-    ignore_changes = [kubernetes_version]
-  }
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks_test01.kube_config_raw
+output "resource_group_id" {
+  value = azurerm_resource_group.mo_test.id
 }
 ```
