66e6308413ec4725ace27d04daf6f5b1b7a777ad
mosk-anw <mo.sk@appnetwise.com>
2025-08-10 09:45:58 +0100

    feat: Add terraform config for 'resource group... '

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -3,27 +3,27 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0"
+      version = "~> 3.0"
     }
   }
 
-  required_version = ">= 1.0"
+  required_version = ">= 1.1.0"
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg_storage" {
-  name     = "rg-storage"
+resource "azurerm_resource_group" "test_rg" {
+  name     = "test-rg"
   location = "UK South"
 }
 
-resource "azurerm_storage_account" "test_storage" {
-  name                     = "teststorageacct"
-  resource_group_name      = azurerm_resource_group.rg_storage.name
-  location                 = azurerm_resource_group.rg_storage.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+output "resource_group_id" {
+  value = azurerm_resource_group.test_rg.id
 }
 ```
