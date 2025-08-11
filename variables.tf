644f8fd60a9657ffa82ea54c76a3a44c2eb1d857
mosk-anw <mo.sk@appnetwise.com>
2025-08-11 21:57:25 +0100

    feat: Add terraform config for 'create a resource group'

--- variables.tf
+++ variables.tf
@@ -1,37 +1,11 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "testrg786"
+  default     = "myrg"
 }
 
-variable "location" {
-  description = "The Azure region where the resources will be created"
+variable "resource_group_location" {
+  description = "The location of the resource group"
   type        = string
-  default     = "asia"
-}
-
-variable "vm_name" {
-  description = "The name of the virtual machine"
-  type        = string
-  default     = "testvm"
-}
-
-variable "admin_username" {
-  description = "The admin username for the Linux VM"
-  type        = string
-  default     = "azureuser"
-}
-
-variable "ssh_public_key" {
-  description = "The SSH public key for the Linux VM"
-  type        = string
-}
-
-variable "tags" {
-  description = "Tags to be applied to resources"
-  type        = map(string)
-  default     = {
-    environment = "test"
-    project     = "web-hosting"
-  }
+  default     = "eastus"
 }
