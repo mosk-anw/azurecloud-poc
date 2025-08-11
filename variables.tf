c54c1241ac03a8640e9b48c6bffa5cef461a085b
mosk-anw <mo.sk@appnetwise.com>
2025-08-11 21:38:32 +0100

    feat: Add terraform config for 'a Linux VM with Ubuntu and public IP for web hosting'

--- variables.tf
+++ variables.tf
@@ -1,22 +1,37 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
+  default     = "testrg786"
 }
 
 variable "location" {
   description = "The Azure region where the resources will be created"
   type        = string
-  default     = "eastus"
+  default     = "asia"
 }
 
-variable "index_document" {
-  description = "The index document for the static website"
+variable "vm_name" {
+  description = "The name of the virtual machine"
   type        = string
-  default     = "index.html"
+  default     = "testvm"
 }
 
-variable "error_document" {
-  description = "The error document for the static website"
+variable "admin_username" {
+  description = "The admin username for the Linux VM"
   type        = string
-  default     = "error.html"
+  default     = "azureuser"
+}
+
+variable "ssh_public_key" {
+  description = "The SSH public key for the Linux VM"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to be applied to resources"
+  type        = map(string)
+  default     = {
+    environment = "test"
+    project     = "web-hosting"
+  }
 }
