03ac50b738bc2b8b386ccd5d0c1a7bf7439be79a
mosk-anw <mo.sk@appnetwise.com>
2025-08-11 18:52:37 +0100

    feat: Add terraform config for 'create a kubernetes cluster with autoscaling and managed idenity enabled'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -12,111 +12,28 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.resource_group_name}-vnet"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  address_space       = ["10.0.0.0/16"]
-}
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "myaks01"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = var.dns_prefix
 
-resource "azurerm_subnet" "main" {
-  name                 = "default"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
 
-resource "azurerm_network_security_group" "vm_nsg" {
-  name                = "${var.resource_group_name}-vm-nsg"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+    min_count = 1
+    max_count = 3
   }
 
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  tags = var.tags
-}
-
-resource "azurerm_network_interface" "vm_nic" {
-  name                = "${var.resource_group_name}-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.main.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
-  }
-
-  tags = var.tags
-}
-
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "${var.resource_group_name}-pip"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  allocation_method   = "Dynamic"
-
-  tags = var.tags
-}
-
-resource "azurerm_network_interface_security_group_association" "vm_nsg_association" {
-  network_interface_id      = azurerm_network_interface.vm_nic.id
-  network_security_group_id = azurerm_network_security_group.vm_nsg.id
-}
-
-resource "azurerm_linux_virtual_machine" "main" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  size                = var.vm_size
-
-  admin_username                  = var.admin_username
-  disable_password_authentication = true
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
-  }
-
-  network_interface_ids = [azurerm_network_interface.vm_nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20_04-lts"
-    version   = "latest"
+  identity {
+    type = "SystemAssigned"
   }
 
   tags = var.tags
--- variables.tf
+++ variables.tf
@@ -1,37 +1,23 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
+  default     = "myeks01"
 }
 
 variable "location" {
-  description = "The Azure region to deploy resources"
+  description = "The Azure region where the resources will be deployed"
   type        = string
-  default     = "eastus"
+  default     = "ukwest"
 }
 
-variable "vm_name" {
-  description = "The name of the virtual machine"
+variable "dns_prefix" {
+  description = "The DNS prefix for the AKS cluster"
   type        = string
-}
-
-variable "vm_size" {
-  description = "The size of the virtual machine"
-  type        = string
-  default     = "Standard_B1s"
-}
-
-variable "admin_username" {
-  description = "The admin username for the virtual machine"
-  type        = string
-}
-
-variable "ssh_public_key" {
-  description = "The SSH public key for the virtual machine"
-  type        = string
+  default     = "myaks"
 }
 
 variable "tags" {
-  description = "Tags to be applied to resources"
+  description = "A map of tags to assign to the resources"
   type        = map(string)
   default     = {}
 }
