143d29e30bd9d3f70e4c1ea8b6cc32345f5478ee
mosk-anw <mo.sk@appnetwise.com>
2025-08-11 14:51:53 +0100

    feat: Add terraform config for 'new aks cluster'

--- variables.tf
+++ variables.tf
@@ -1,17 +1,15 @@
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "The name of the resource group for the AKS cluster."
   type        = string
-  default     = "production-rg"
 }
 
 variable "location" {
-  description = "The Azure region where the resource group should be created"
+  description = "The Azure region where the resource group should be created."
   type        = string
-  default     = "uksouth"
+  default     = "eastus"
 }
 
-variable "tags" {
-  description = "A map of tags to assign to the resource group"
-  type        = map(string)
-  default     = {}
+variable "aks_cluster_name" {
+  description = "The name of the AKS cluster."
+  type        = string
 }
