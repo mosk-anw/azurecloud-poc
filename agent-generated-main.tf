3746a3d40b1a01922b145f57cfa574a018031931
mosk-anw <mo.sk@appnetwise.com>
2025-08-11 19:23:57 +0100

    feat: Add terraform config for 'up an AKS cluster with 3 nodes for microservices'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -18,23 +18,25 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "myaks01"
+  name                = var.aks_cluster_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   dns_prefix          = var.dns_prefix
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 3
     vm_size    = "Standard_DS2_v2"
-
-    min_count = 1
-    max_count = 3
+    min_count  = 1
+    max_count  = 5
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  tags = var.tags
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "The name of the resource group."
   type        = string
-  default     = "myeks01"
+  default     = "testrg"
 }
 
 variable "location" {
-  description = "The Azure region where the resources will be deployed"
+  description = "The location of the resource group."
   type        = string
-  default     = "ukwest"
+  default     = "india"
+}
+
+variable "aks_cluster_name" {
+  description = "The name of the AKS cluster."
+  type        = string
+  default     = "example-aks"
 }
 
 variable "dns_prefix" {
-  description = "The DNS prefix for the AKS cluster"
+  description = "The DNS prefix for the AKS cluster."
   type        = string
-  default     = "myaks"
-}
-
-variable "tags" {
-  description = "A map of tags to assign to the resources"
-  type        = map(string)
-  default     = {}
+  default     = "example"
 }
