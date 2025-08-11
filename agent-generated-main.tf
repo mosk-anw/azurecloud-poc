45ea89e50724df7a3e15a4bb935b306662fe08d6
mosk-anw <mo.sk@appnetwise.com>
2025-08-11 22:19:42 +0100

    feat: Add terraform config for 'aks with maaged identity and autoscaling 2 zones'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -11,26 +11,23 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "test123"
-  location = "uksouth"
-}
-
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "exampleAKSCluster"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = var.dns_prefix
-
-  default_node_pool {
-    name       = "default"
-    node_count = 3
-    vm_size    = "Standard_DS2_v2"
-    min_count  = 1
-    max_count  = 5
-  }
+  name                = "test456"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   identity {
     type = "SystemAssigned"
   }
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+    min_count  = 1
+    max_count  = 3
+    zones      = ["1", "2"]
+  }
+
+  dns_prefix = var.dns_prefix
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,16 @@
+variable "location" {
+  description = "The Azure region where the AKS cluster will be deployed."
+  type        = string
+  default     = "eastasia"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the AKS cluster."
+  type        = string
+}
+
 variable "dns_prefix" {
   description = "The DNS prefix to use with the AKS cluster."
   type        = string
-  default     = "exampledns"
+  default     = "test456"
 }
