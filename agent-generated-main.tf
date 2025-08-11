b668fb29d4188a0d49b3fdedd2d38665aec9575e
mosk-anw <mo.sk@appnetwise.com>
2025-08-11 14:21:29 +0100

    feat: Add terraform config for 'a Linux VM with Ubuntu and public IP for web hosting'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -12,9 +12,107 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "production_rg" {
+resource "azurerm_resource_group" "vmtest01" {
   name     = var.resource_group_name
   location = var.location
+}
+
+resource "azurerm_virtual_network" "vm_vnet" {
+  name                = "${var.resource_group_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.vmtest01.location
+  resource_group_name = azurerm_resource_group.vmtest01.name
+}
+
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.vmtest01.name
+  virtual_network_name = azurerm_virtual_network.vm_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "${var.resource_group_name}-nic"
+  location            = azurerm_resource_group.vmtest01.location
+  resource_group_name = azurerm_resource_group.vmtest01.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+  }
+}
+
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "${var.resource_group_name}-pip"
+  resource_group_name = azurerm_resource_group.vmtest01.name
+  location            = azurerm_resource_group.vmtest01.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "${var.resource_group_name}-vm-nsg"
+  location            = azurerm_resource_group.vmtest01.location
+  resource_group_name = azurerm_resource_group.vmtest01.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "vm_nsg_association" {
+  network_interface_id      = azurerm_network_interface.vm_nic.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.vmtest01.name
+  location            = azurerm_resource_group.vmtest01.location
+  size                = var.vm_size
+
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
+
+  admin_username                  = var.admin_username
+  disable_password_authentication = true
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
 
   tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,40 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "production-rg"
+  default     = "vmtest01"
 }
 
 variable "location" {
-  description = "The Azure region where the resource group should be created"
+  description = "The Azure region where the resources will be created"
   type        = string
-  default     = "uksouth"
+  default     = "ukwest"
+}
+
+variable "vm_name" {
+  description = "The name of the virtual machine"
+  type        = string
+  default     = "vmtest01"
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machine"
+  type        = string
+  default     = "Standard_B1s"
+}
+
+variable "admin_username" {
+  description = "The admin username for the virtual machine"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key" {
+  description = "The SSH public key for the virtual machine"
+  type        = string
 }
 
 variable "tags" {
-  description = "A map of tags to assign to the resource group"
+  description = "A map of tags to assign to the resources"
   type        = map(string)
   default     = {}
 }
