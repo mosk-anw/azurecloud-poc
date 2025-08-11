8ae6be59cc9e9b5e2eefdb77db66356ebd826b58
mosk-anw <mo.sk@appnetwise.com>
2025-08-11 14:29:57 +0100

    feat: Add terraform config for 'create aks cluster'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -12,9 +12,26 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "production_rg" {
+resource "azurerm_resource_group" "aks_rg" {
   name     = var.resource_group_name
   location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "${var.aks_cluster_name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 
   tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,35 @@
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "The name of the resource group."
   type        = string
-  default     = "production-rg"
+  default     = "myaks678"
 }
 
 variable "location" {
-  description = "The Azure region where the resource group should be created"
+  description = "The Azure region where the resources will be created."
   type        = string
-  default     = "uksouth"
+  default     = "india"
+}
+
+variable "aks_cluster_name" {
+  description = "The name of the AKS cluster."
+  type        = string
+  default     = "myaks678-cluster"
+}
+
+variable "node_count" {
+  description = "The number of nodes in the default node pool."
+  type        = number
+  default     = 3
+}
+
+variable "vm_size" {
+  description = "The size of the Virtual Machine instances in the AKS cluster."
+  type        = string
+  default     = "Standard_DS2_v2"
 }
 
 variable "tags" {
-  description = "A map of tags to assign to the resource group"
+  description = "A map of tags to assign to the resources."
   type        = map(string)
   default     = {}
 }
