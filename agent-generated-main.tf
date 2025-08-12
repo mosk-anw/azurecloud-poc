bd3735d9716ea40ee12ee562fcaf1b48562c0649
mosk-anw <mo.sk@appnetwise.com>
2025-08-12 09:50:20 +0100

    feat: Add terraform config for 'up an AKS cluster with 3 nodes for microservices'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -5,14 +5,31 @@ terraform {
       version = ">= 3.71.0, < 5.0.0"
     }
   }
-  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "production" {
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "newaks786"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_DS2_v2"
+    zones      = ["1", "2", "3"]
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,16 @@
-variable "resource_group_name" {
-  description = "The name of the resource group."
+variable "location" {
+  description = "The Azure region where the AKS cluster will be deployed."
   type        = string
-  default     = "production-rg"
+  default     = "South India"
 }
 
-variable "location" {
-  description = "The Azure region where the resource group should be created."
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the AKS cluster."
   type        = string
-  default     = "UK South"
+}
+
+variable "dns_prefix" {
+  description = "The DNS prefix to use with the AKS cluster."
+  type        = string
+  default     = "newaks786"
 }
