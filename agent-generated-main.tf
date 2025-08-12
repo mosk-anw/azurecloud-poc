126dcbd0be68643bf5f7608ec6af64a158e253e7
mosk-anw <mo.sk@appnetwise.com>
2025-08-12 10:05:47 +0100

    feat: Add terraform config for 'i want to create a new vm in a private network and accesible via bastion in uksouth region name it as vmprivate01 , make it auto scalable'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -11,24 +11,145 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_kubernetes_cluster" "myaks01" {
-  name                = "myaks01"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  dns_prefix          = var.dns_prefix
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-vmprivate01"
+  location = "uksouth"
+}
 
-  default_node_pool {
-    name       = "default"
-    node_count = 10
-    vm_size    = "Standard_DS2_v2"
-    min_count  = 1
-    max_count  = 15
-    zones      = ["1", "2", "3"]
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-vmprivate01"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-vmprivate01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "bastion-vmprivate01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
+  }
+}
+
+resource "azurerm_public_ip" "bastion_public_ip" {
+  name                = "bastion-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-vmprivate01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vmprivate01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                = "Standard_D2s_v5"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  identity {
-    type = "SystemAssigned"
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 
-  tags = var.tags
+  admin_username = "adminuser"
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = var.ssh_public_key
+  }
+
+  zone = "1"
+}
+
+resource "azurerm_monitor_autoscale_setting" "autoscale" {
+  name                = "autoscale-vmprivate01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  target_resource_id  = azurerm_linux_virtual_machine.vm.id
+
+  profile {
+    name = "default"
+
+    capacity {
+      minimum = 1
+      maximum = 5
+      default = 1
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine.vm.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 80
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine.vm.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 20
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,22 +1,4 @@
-variable "location" {
-  description = "The Azure region where the AKS cluster will be deployed."
+variable "ssh_public_key" {
+  description = "The SSH public key for the VM."
   type        = string
-  default     = "uksouth"
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the AKS cluster."
-  type        = string
-}
-
-variable "dns_prefix" {
-  description = "The DNS prefix to use with the AKS cluster."
-  type        = string
-  default     = "myaks01dns"
-}
-
-variable "tags" {
-  description = "A map of tags to assign to the resources."
-  type        = map(string)
-  default     = {}
 }
