1dd014aea6504707e747ed0a216544b931cf59c3
mosk-anw <mo.sk@appnetwise.com>
2025-08-12 09:21:45 +0100

    feat: Add terraform config for 'a resource group called 'production-rg' in UK South'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -5,29 +5,14 @@ terraform {
       version = ">= 3.71.0, < 5.0.0"
     }
   }
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "test456"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-    min_count  = 1
-    max_count  = 3
-    zones      = ["1", "2"]
-  }
-
-  dns_prefix = var.dns_prefix
+resource "azurerm_resource_group" "production" {
+  name     = var.resource_group_name
+  location = var.location
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,11 @@
-variable "location" {
-  description = "The Azure region where the AKS cluster will be deployed."
-  type        = string
-  default     = "eastasia"
-}
-
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the AKS cluster."
+  description = "The name of the resource group to be created."
   type        = string
+  default     = "production-rg"
 }
 
-variable "dns_prefix" {
-  description = "The DNS prefix to use with the AKS cluster."
+variable "location" {
+  description = "The Azure region where the resource group should be created."
   type        = string
-  default     = "test456"
+  default     = "uksouth"
 }
