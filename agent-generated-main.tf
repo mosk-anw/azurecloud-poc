7553048ec4fbaf6d741e13b46d4a200b309f5e96
mosk-anw <mo.sk@appnetwise.com>
2025-08-18 18:08:01 +0100

    feat: Add terraform config for 'create a secure vm for hosting a website and basition host to mgmt acess it but close all the port on vm only https'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = ">= 3.71.0, < 5.0.0"
     }
   }
 }
@@ -11,52 +11,68 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "myweb01" {
-  name     = "myweb01"
-  location = var.location
+resource "azurerm_resource_group" "mywebrg" {
+  name     = "mywebrg"
+  location = "South India"
 }
 
-resource "azurerm_public_ip" "myweb01_pip" {
-  name                = "myweb01-pip"
-  resource_group_name = azurerm_resource_group.myweb01.name
-  location            = azurerm_resource_group.myweb01.location
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_interface" "myweb01_nic" {
-  name                = "myweb01-nic"
-  location            = azurerm_resource_group.myweb01.location
-  resource_group_name = azurerm_resource_group.myweb01.name
-
-  ip_configuration {
-    name                          = "myweb01-ipconfig"
-    subnet_id                     = azurerm_subnet.myweb01_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.myweb01_pip.id
-  }
-}
-
-resource "azurerm_virtual_network" "myweb01_vnet" {
-  name                = "myweb01-vnet"
+resource "azurerm_virtual_network" "myweb_vnet" {
+  name                = "myweb-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.myweb01.location
-  resource_group_name = azurerm_resource_group.myweb01.name
+  location            = azurerm_resource_group.mywebrg.location
+  resource_group_name = azurerm_resource_group.mywebrg.name
 }
 
-resource "azurerm_subnet" "myweb01_subnet" {
-  name                 = "myweb01-subnet"
-  resource_group_name  = azurerm_resource_group.myweb01.name
-  virtual_network_name = azurerm_virtual_network.myweb01_vnet.name
+resource "azurerm_subnet" "myweb_subnet" {
+  name                 = "myweb-subnet"
+  resource_group_name  = azurerm_resource_group.mywebrg.name
+  virtual_network_name = azurerm_virtual_network.myweb_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_linux_virtual_machine" "myweb01_vm" {
-  name                = "myweb01-vm"
-  resource_group_name = azurerm_resource_group.myweb01.name
-  location            = azurerm_resource_group.myweb01.location
-  size                = "Standard_D2s_v5"
-  admin_username      = var.admin_username
-  network_interface_ids = [azurerm_network_interface.myweb01_nic.id]
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.mywebrg.name
+  virtual_network_name = azurerm_virtual_network.myweb_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "myweb_nsg" {
+  name                = "myweb-nsg"
+  location            = azurerm_resource_group.mywebrg.location
+  resource_group_name = azurerm_resource_group.mywebrg.name
+
+  security_rule {
+    name                       = "Allow-HTTPS-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "myweb_nic" {
+  name                = "myweb-nic"
+  location            = azurerm_resource_group.mywebrg.location
+  resource_group_name = azurerm_resource_group.mywebrg.name
+
+  ip_configuration {
+    name                          = "myweb-ipconfig"
+    subnet_id                     = azurerm_subnet.myweb_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "myweb_vm" {
+  name                = "myweb-vm"
+  location            = azurerm_resource_group.mywebrg.location
+  resource_group_name = azurerm_resource_group.mywebrg.name
+  network_interface_ids = [azurerm_network_interface.myweb_nic.id]
+  size                = "Standard_B1s"
 
   os_disk {
     caching              = "ReadWrite"
@@ -70,10 +86,35 @@ resource "azurerm_linux_virtual_machine" "myweb01_vm" {
     version   = "latest"
   }
 
+  admin_username = "azureuser"
   admin_ssh_key {
-    username   = var.admin_username
+    username   = "azureuser"
     public_key = var.ssh_public_key
   }
 
-  tags = var.tags
+  disable_password_authentication = true
+}
+
+resource "azurerm_public_ip" "bastion_pip" {
+  name                = "bastion-pip"
+  resource_group_name = azurerm_resource_group.mywebrg.name
+  location            = azurerm_resource_group.mywebrg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_bastion_host" "myweb_bastion" {
+  name                = "myweb-bastion"
+  location            = azurerm_resource_group.mywebrg.location
+  resource_group_name = azurerm_resource_group.mywebrg.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "myweb_nic_nsg" {
+  network_interface_id      = azurerm_network_interface.myweb_nic.id
+  network_security_group_id = azurerm_network_security_group.myweb_nsg.id
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,4 @@
-variable "location" {
-  description = "The Azure region to deploy resources in."
-  type        = string
-  default     = "southindia"
-}
-
-variable "admin_username" {
-  description = "The admin username for the virtual machine."
-  type        = string
-  default     = "azureuser"
-}
-
 variable "ssh_public_key" {
-  description = "The SSH public key for accessing the virtual machine."
+  description = "The SSH public key for the VM."
   type        = string
-}
-
-variable "tags" {
-  description = "Tags to apply to resources."
-  type        = map(string)
-  default     = {
-    environment = "development"
-    project     = "static-website"
-  }
 }
