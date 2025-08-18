805c6c91f0fc51194e32d17e0e24314eca8104f4
Mo <154330787+mosk-anw@users.noreply.github.com>
2025-08-19 00:34:29 +0100

    Add/update terraform/main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+# Terraform configuration for Azure Resource Group
+# Following Azure Terraform best practices
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Outputs
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "resource_group_location" {
+  description = "Location of the resource group"
+  value       = azurerm_resource_group.main.location
+}
+
+output "resource_group_id" {
+  description = "ID of the resource group"
+  value       = azurerm_resource_group.main.id
+}
+
+output "azure_portal_link" {
+  description = "Direct link to the resource group in Azure Portal"
+  value       = "https://portal.azure.com/#@${var.tenant_id}/resource${azurerm_resource_group.main.id}"
+  sensitive   = true
+}
