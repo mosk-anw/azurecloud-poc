ed68b24beeafecdd30483c85136e84175a446f70
mosk-anw <mo.sk@appnetwise.com>
2025-08-25 12:26:05 +0100

    feat: Add terraform config for 'up an AKS cluster with 3 nodes for microservices'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -11,21 +11,21 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = var.location
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "myResourceGroup"
+  location = "East US"
 }
 
 module "aks_cluster" {
-  source              = "Azure/avm-res-containerservice-managedcluster/azurerm"
-  version             = "~> 0.2"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = var.location
-  dns_prefix          = "myakscluster"
+  source  = "Azure/avm-res-containerservice-managedcluster/azurerm"
+  version = "~> 0.2"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = azurerm_resource_group.aks_rg.location
   default_node_pool   = {
     name       = "default"
-    node_count = 2
+    node_count = 3
     vm_size    = "Standard_DS2_v2"
   }
-  name = "myakscluster"
+  name       = "testaks"
+  dns_prefix = "testaks"
 }
