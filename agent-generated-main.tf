908588cfb7fb2d48b9f6cd016a4ddd914598cfd1
mosk-anw <mo.sk@appnetwise.com>
2025-08-25 12:20:26 +0100

    feat: Add terraform config for 'create a new aks cluster of 2 nodes in india region'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -16,59 +16,16 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+module "aks_cluster" {
+  source              = "Azure/avm-res-containerservice-managedcluster/azurerm"
+  version             = "~> 0.2"
   resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "example-publicip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+  location            = var.location
+  dns_prefix          = "myakscluster"
+  default_node_pool   = {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
   }
-}
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-vm"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_DS1_v2"
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-
-  network_interface_ids = [azurerm_network_interface.example.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
+  name = "myakscluster"
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,4 @@
 variable "location" {
-  description = "The location where the resources will be provisioned."
-  type        = string
-}
-
-variable "admin_username" {
-  description = "The admin username for the virtual machine."
-  type        = string
-}
-
-variable "admin_password" {
-  description = "The admin password for the virtual machine."
+  description = "The Azure region where the AKS cluster will be created."
   type        = string
 }
