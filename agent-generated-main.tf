7a331c09c8fddb48866f8304cfc4ea2d34cd6fda
mosk-anw <mo.sk@appnetwise.com>
2025-08-25 13:34:02 +0100

    feat: Add terraform config for 'Deploy Ubuntu VM with public IP'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -1,26 +1,75 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.46"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "myResourceGroup"
+resource "azurerm_resource_group" "testvm_rg" {
+  name     = "testvm"
   location = "uksouth"
 }
 
-resource "azurerm_kubernetes_cluster" "mycluster01" {
-  name                = "mycluster01"
-  location            = azurerm_resource_group.myresourcegroup.location
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  dns_prefix          = "mycluster01"
+resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
+  name                = "ubuntu-vm"
+  resource_group_name = azurerm_resource_group.testvm_rg.name
+  location            = azurerm_resource_group.testvm_rg.location
+  size                = "Standard_DS1_v2"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 3
-    vm_size    = "Standard_DS2_v2"
-    zones      = ["1", "2", "3"]
+  admin_username = "adminuser"
+  admin_password = "Password1234!"
+
+  network_interface_ids = [azurerm_network_interface.ubuntu_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  identity {
-    type = "SystemAssigned"
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
+}
+
+resource "azurerm_public_ip" "ubuntu_public_ip" {
+  name                = "ubuntu-public-ip"
+  location            = azurerm_resource_group.testvm_rg.location
+  resource_group_name = azurerm_resource_group.testvm_rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "ubuntu_nic" {
+  name                = "ubuntu-nic"
+  location            = azurerm_resource_group.testvm_rg.location
+  resource_group_name = azurerm_resource_group.testvm_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.testvm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ubuntu_public_ip.id
+  }
+}
+
+resource "azurerm_subnet" "testvm_subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.testvm_rg.name
+  virtual_network_name = azurerm_virtual_network.testvm_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_virtual_network" "testvm_vnet" {
+  name                = "testvm-vnet"
+  resource_group_name = azurerm_resource_group.testvm_rg.name
+  location            = azurerm_resource_group.testvm_rg.location
+  address_space       = ["10.0.0.0/16"]
 }
