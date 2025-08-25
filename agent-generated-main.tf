842c1812fbe05f3e2594a80b4b5af5602d947ade
mosk-anw <mo.sk@appnetwise.com>
2025-08-25 13:22:23 +0100

    feat: Add terraform config for 'Setup PostgreSQL database'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -3,18 +3,18 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "myResourceGroup"
+  name     = "example-resources"
   location = "uksouth"
 }
 
 resource "azurerm_postgresql_server" "example" {
-  name                = "mydb01"
+  name                = "testdb01"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku_name            = "B_Gen5_1"
+  version             = "11"
+  ssl_enforcement_enabled = true
   storage_mb          = 5120
   administrator_login = "myadmin"
-  administrator_login_password = "P@ssw0rd1234!"
-  ssl_enforcement_enabled = true
-  version = "11"
+  administrator_login_password = "P@ssw0rd"
 }
