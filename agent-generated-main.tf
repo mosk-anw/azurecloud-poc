11b8d0bcc8fb4f6945f2592328cb25105f8620cc
mosk-anw <mo.sk@appnetwise.com>
2025-08-25 11:50:10 +0100

    feat: Add terraform config for 'create a new virtual machine in private network with bastion host but allow 443 on virtual machine for website'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -11,94 +11,64 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "private_network_rg" {
-  name     = "private-network-rg"
-  location = "India"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.location
 }
 
-resource "azurerm_virtual_network" "private_network_vnet" {
-  name                = "private-network-vnet"
-  resource_group_name = azurerm_resource_group.private_network_rg.name
-  location            = azurerm_resource_group.private_network_rg.location
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "private_subnet" {
-  name                 = "private-subnet"
-  resource_group_name  = azurerm_resource_group.private_network_rg.name
-  virtual_network_name = azurerm_virtual_network.private_network_vnet.name
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "bastion_public_ip" {
-  name                = "bastion-public-ip"
-  location            = azurerm_resource_group.private_network_rg.location
-  resource_group_name = azurerm_resource_group.private_network_rg.name
+resource "azurerm_public_ip" "example" {
+  name                = "example-publicip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
 }
 
-resource "azurerm_network_security_group" "bastion_nsg" {
-  name                = "bastion-nsg"
-  location            = azurerm_resource_group.private_network_rg.location
-  resource_group_name = azurerm_resource_group.private_network_rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface" "bastion_nic" {
-  name                      = "bastion-nic"
-  location                  = azurerm_resource_group.private_network_rg.location
-  resource_group_name       = azurerm_resource_group.private_network_rg.name
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.private_subnet.id
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.bastion_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
-resource "azurerm_virtual_machine" "bastion_vm" {
-  name                  = "bastion-vm"
-  location              = azurerm_resource_group.private_network_rg.location
-  resource_group_name   = azurerm_resource_group.private_network_rg.name
-  network_interface_ids = [azurerm_network_interface.bastion_nic.id]
-  vm_size               = "Standard_B1s"
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_DS1_v2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
 
-  storage_image_reference {
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "bastion-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
-  }
-
-  os_profile {
-    computer_name  = "bastion-vm"
-    admin_username = "adminuser"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path     = "/home/adminuser/.ssh/authorized_keys"
-      key_data = "${var.admin_ssh_public_key}"
-    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
-variable "admin_ssh_public_key" {
-  description = "The SSH public key to be used for authentication"
+variable "location" {
+  description = "The location where the resources will be provisioned."
+  type        = string
+}
+
+variable "admin_username" {
+  description = "The admin username for the virtual machine."
+  type        = string
+}
+
+variable "admin_password" {
+  description = "The admin password for the virtual machine."
   type        = string
 }
