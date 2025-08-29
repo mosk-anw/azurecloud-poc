e796451ec03ea3586ab68dd8720412acd46a9cc4
mosk-anw <mo.sk@appnetwise.com>
2025-08-29 23:07:58 +0100

    feat: Add terraform config for 'new resource group rg45678 in india central region'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -1,17 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.46"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "test4567rg"
-  location = "uksouth"
+  name     = "rg45678"
+  location = "centralindia"
 }
