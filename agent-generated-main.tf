eb3872030b7a4f84e3026e3c46914374eb04d5ca
mosk-anw <mo.sk@appnetwise.com>
2025-08-29 22:07:57 +0100

    feat: Add terraform config for 'Create a new resource group called demo-rg in East US'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -11,21 +11,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
-  location = "uksouth"
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "teststorage01"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "web" {
-  name                  = "web"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
+resource "azurerm_resource_group" "demo_rg" {
+  name     = "demo-rg"
+  location = "East US"
 }
