1106af96a6a6bec35c46a90c53a215e504d588c9
mosk-anw <mo.sk@appnetwise.com>
2025-08-30 09:06:39 +0100

    feat: Add terraform config for 'Create a resource group in UK South'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -1,8 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.46"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "rg45678"
-  location = "centralindia"
+  name     = "testrg786"
+  location = "uksouth"
 }
