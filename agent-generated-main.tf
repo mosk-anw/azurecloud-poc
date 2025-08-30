43eefbbfabde77c30a86bb9df9461f567fb31d17
mosk-anw <mo.sk@appnetwise.com>
2025-08-30 12:00:37 +0100

    feat: Add terraform config for 'create. a new rg'

--- agent-generated-main.tf
+++ agent-generated-main.tf
@@ -12,6 +12,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "testrg786"
-  location = "uksouth"
+  name     = "myResourceGroup"
+  location = var.location
 }
