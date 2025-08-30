0e5a6730d193fbb752ffd942bacf435166bf63b4
mosk-anw <mo.sk@appnetwise.com>
2025-08-30 22:29:51 +0100

    feat: Add terraform config for 'Create a resource group in UK South'

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
-variable "tags" {
-  description = "A map of tags to assign to the resources"
-  type        = map(string)
-  default     = {}
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created"
+  type        = string
+  default     = "UK South"
 }
