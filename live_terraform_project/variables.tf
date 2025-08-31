9a9c12a27a21aa3678ef2737c37f8c6fc759f6f6
mosk-anw <mo.sk@appnetwise.com>
2025-08-31 21:47:37 +0100

    feat: Add terraform config for 'Create a resource group in UK South'

--- live_terraform_project/variables.tf
+++ live_terraform_project/variables.tf
@@ -1,14 +1,9 @@
-# Variables for this Terraform configuration
-# Edit these values according to your requirements
-
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "default-rg"
 }
 
 variable "location" {
   description = "The Azure region where resources will be created"
   type        = string
-  default     = "eastus"
 }
