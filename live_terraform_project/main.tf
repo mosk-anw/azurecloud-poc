1bfcf06c386d21fdf5d93f37b9cb50e54fa2fa94
mosk-anw <mo.sk@appnetwise.com>
2025-08-31 20:10:14 +0100

    feat: Add terraform config for 'create a new RG'

--- live_terraform_project/main.tf
+++ live_terraform_project/main.tf
@@ -2,17 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 2.0"
     }
   }
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.13"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example_rg" {
-  name     = "rgagenttest123"
+resource "azurerm_resource_group" "example" {
+  name     = "devopsagentrg777"
   location = "UK South"
 }
