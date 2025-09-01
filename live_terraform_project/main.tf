819f28e2436f7ead21664e76195c25091a14f8af
mosk-anw <mo.sk@appnetwise.com>
2025-09-01 07:04:30 +0100

    feat: Add terraform config for 'create a new resource group'

--- live_terraform_project/main.tf
+++ live_terraform_project/main.tf
@@ -1,11 +1,10 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source = "hashicorp/azurerm"
+      version = ">= 2.46"
     }
   }
-  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
@@ -13,6 +12,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "rg-devops-agent-test123"
+  name     = "devops-agent-test-rg01"
   location = "UK South"
 }
