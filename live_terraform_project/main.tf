16667f695039c86572202bb14300fd553919e7e3
mosk-anw <mo.sk@appnetwise.com>
2025-09-02 19:24:34 +0100

    feat: Add terraform config for 'create a resource group in europe'

--- live_terraform_project/main.tf
+++ live_terraform_project/main.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.9"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.46"
+      version = "~> 3.0"
     }
   }
 }
@@ -15,6 +17,8 @@ module "resource_group" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
   version = "~> 0.1"
 
-  name     = "testrg0178"
-  location = "UK South"
+  name           = "testeurop"
+  location       = "West Europe"
+  enable_telemetry = true
+  tags = var.tags
 }
--- live_terraform_project/variables.tf
+++ live_terraform_project/variables.tf
@@ -1,14 +1,19 @@
-# Variables for this Terraform configuration
-# Edit these values according to your requirements
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Resource tags"
+  type        = map(string)
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
 
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "default-rg"
-}
-
-variable "location" {
-  description = "The Azure region where resources will be created"
-  type        = string
-  default     = "eastus"
 }
