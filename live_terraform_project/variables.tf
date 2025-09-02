1a00782f4b7898c950832962e31de704890878e2
mosk-anw <mo.sk@appnetwise.com>
2025-09-02 23:00:25 +0100

    feat: Add terraform config for 'create resource group e2etest3 in east us'

--- live_terraform_project/variables.tf
+++ live_terraform_project/variables.tf
@@ -1,5 +1,10 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
 variable "tags" {
-  description = "A map of tags to assign to the resources"
+  description = "Resource tags"
   type        = map(string)
   default     = {}
 }
