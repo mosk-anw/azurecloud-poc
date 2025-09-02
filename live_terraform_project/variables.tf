04d48280027b50cebd975f3641d9397b5bc06db0
mosk-anw <mo.sk@appnetwise.com>
2025-09-02 19:24:07 +0100

    feat: Add terraform config for 'create a basic resource group'

--- live_terraform_project/variables.tf
+++ live_terraform_project/variables.tf
@@ -1,14 +1,14 @@
-# Variables for this Terraform configuration
-# Edit these values according to your requirements
+variable "tags" {
+  description = "Resource tags"
+  type        = map(string)
+}
 
-variable "resource_group_name" {
-  description = "The name of the resource group"
+variable "environment" {
+  description = "Environment name"
   type        = string
-  default     = "default-rg"
 }
 
 variable "location" {
-  description = "The Azure region where resources will be created"
+  description = "Azure region"
   type        = string
-  default     = "eastus"
 }
