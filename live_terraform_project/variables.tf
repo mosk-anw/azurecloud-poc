19f3928e1eea0e31a3d64651261187800511dd74
mosk-anw <mo.sk@appnetwise.com>
2025-09-02 18:06:05 +0100

    feat: Add terraform config for 'create a resource group for testing clean PR workflow'

--- live_terraform_project/variables.tf
+++ live_terraform_project/variables.tf
@@ -1,14 +1,15 @@
-# Variables for this Terraform configuration
-# Edit these values according to your requirements
+variable "location" {
+  description = "The Azure region where resources will be created"
+  type        = string
+}
 
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "default-rg"
 }
 
-variable "location" {
-  description = "The Azure region where resources will be created"
-  type        = string
-  default     = "eastus"
+variable "tags" {
+  description = "A map of tags to assign to the resources"
+  type        = map(string)
+  default     = {}
 }
