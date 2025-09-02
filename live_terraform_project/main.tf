7ff20e09e17ee22a2007fb520dd2bc973570b37f
mosk-anw <mo.sk@appnetwise.com>
2025-09-02 12:23:52 +0100

    feat: Add terraform config for 'create a new rg named rg-dev12345 in region uksouth'

--- live_terraform_project/main.tf
+++ live_terraform_project/main.tf
@@ -1,20 +1,22 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.46"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
   }
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
+# Create Resource Group using AVM
 module "resource_group" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
   version = "~> 0.1"
 
-  name     = "testrg0178"
-  location = "UK South"
+  name     = "rg-dev12345"
+  location = "uksouth"
 }
