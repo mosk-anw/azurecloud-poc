82178d2130adfdde5cb76ea325a03ed872cdd842
mosk-anw <mo.sk@appnetwise.com>
2025-09-03 01:14:43 +0100

    feat: Add terraform config for 'create a new rg named devops-ai-rg-786 in uksouth'

--- live_terraform_project/variables.tf
+++ live_terraform_project/variables.tf
@@ -4,19 +4,16 @@ variable "environment" {
 }
 
 variable "tags" {
-  description = "A map of tags to assign to the resources"
+  description = "Resource tags"
   type        = map(string)
-  default     = {}
 }
 
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "uksouth"
 }
 
 variable "resource_group_base_name" {
   description = "Base name for resource group"
   type        = string
-  default     = "devops-ai"
 }
